bc22723f7178be3ca60f85131e9d57b7f321fe1a
Youssef-Ahmed73 <youssef.nagy073@gmail.com>
2026-05-03 08:29:41 +0300

    gripper urdf modifications

--- src/probot_description/launch/publish_robot_description.launch.py
+++ src/probot_description/launch/publish_robot_description.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 from launch.substitutions import Command
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
-
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
 
@@ -16,21 +16,24 @@ def generate_launch_description():
     robot_description = Command([
         'xacro ',
         xacro_file
-    ])
+    ],on_stderr='warn')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[{
-            'robot_description': robot_description
+            'robot_description': ParameterValue(robot_description, value_type=str)
         }]
     )
 
     joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        output='screen'
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        parameters=[{
+            'robot_description': ParameterValue(robot_description, value_type=str)
+        }]
     )
 
     return LaunchDescription([
